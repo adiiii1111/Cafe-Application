8ed0e2e2607255d32a40ce03e6b52f690f5a339e
Aditya Chutke <aditya.chutke@pharmaace.com>
2025-11-27 13:08:50 +0530

    minor change

---
+++ Backend/app/db/db_connection.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from app import settings
+
+DATABASE_URL = settings.settings.DATABASE_URL
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_size=50,
+    max_overflow=50,
+    pool_timeout=30,
+    pool_recycle=1800,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+app = FastAPI()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
